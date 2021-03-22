0335a28d67bb8f7738bc4e17f56ea0a5aa45bad4
Olivier Perron <olivier.perron12@gmail.com>
2021-03-22 20:18:52 +0100

    Big rework to fit my needs under Fusion 360 with Fusion 360 shortcuts

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,30 @@
-//Author Stefano Sbarra
-//April 2020
+//Author Olivier Perron
+//March 2021
 //FREE FOR ANY NON COMMERCIAL PROJECT
-//Original source: https://www.instructables.com/Space-Mouse-With-Arduino-Micro-Fully-Printable/
+//Original source: https://www.instructables.com/Space-Mouse-With-Arduino-Micro-Fully-Printable/ from Stefano Sbarra, April 2020
 
-//THIS SKETCH MAKES ARDUINO LEONARDO (OR MICRO) WORK AS A SPACE 3D MOUSE FOR INVENTOR AND FUSION 360
-//NORMAL JOYSTICK MOVE ACT AS VIEW ROTATION USING F4 KEY
-//PRESS AND MOVE ACT AS VIEW ZOOM USING F3 KEY
-//QUICK PRESS OF THE JOYSTICK GOES TO HOME VIEW USING F6
-//ADDITIONAL FUNCTIONS AND SWITCHES ARE EASY TO IMPLEMENT (EXAMPLE SLICE VIEW)
+//THIS SKETCH MAKES ARDUINO LEONARDO (OR MICRO) BASED ON ATmega32U4 WORK AS A SPACE 3D MOUSE FOR FUSION 360
+//NORMAL MODE : JOYSTICK MOVE ACT AS ... A NORMAL MOUSE :)
+//PAN MODE VIEW : PAN BY HOLDING MIDDLE MOUSE BUTTON
+//ROTATE MODE VIEW : ROTATION BY HOLDING SHIFT KEY + MIDDLE MOUSE BUTTON
+//PRESS AND MOVE ACT AS VIEW ZOOM BY ROLLING MIDDLE MOUSE BUTTON (WHATEVER THE CURRENT MODE IS)
+//QUICK PRESS OF THE JOYSTICK ZOOMS TO FIT USING F6
+//ADDITIONAL FUNCTIONS AND SWITCHES ARE EASY TO IMPLEMENT
+// BUTTON1 ACTS AS MOUSE LEFT BUTTON
+// BUTTON2 TOGGLES SPACE MOUSE MODE (NORMAL, PAN, ROTATE)
+// BUTTON3 SENDS ESC_KEY
 #include <Arduino.h>
 #include <Mouse.h>
 #include <Keyboard.h>
 
-typedef enum {ROTATE, ZOOM, NORMAL} spacemouse_mode_t;
+typedef enum {NORMAL, PAN, ROTATE, ZOOM} spacemouse_mode_t;
 
 //#define DEBUG
-#define INVERT_MOUSE 1           // Set to -1 to invert
-#define LONG_PRESS_INTERVAL 500  //interval to establish if quick or long press
-#define MOVE_SLOW_DOWN       5   //this slows down the moves
-const int sensitivity [3] = {/*ROTATE*/ 150, /*ZOOM*/150, /*NORMAL*/75}; //Higher sensitivity value = slower mouse, should be <= about 500
-#define MOVE_THRESHOLD 2         // Use of 2 instead of 0 cancels the small fluctuation between joystick position readings
-
-
-//moved and esc key helps to go back from view mode to design
+#define INVERT_MOUSE          0  // 0: Do not invert mouse, 1: Invert mouse
+#define LONG_PRESS_INTERVAL 500  // interval to define if short or long press
+#define MOVE_THRESHOLD        1  // Use of 1 instead of 0 cancels the small fluctuation between joystick position readings. If you experiment instabilities, try to increase to 2
+const int sensitivity [4]    = {/*NORMAL*/ 75, /*PAN*/ 75, /*ROTATE*/150, /*ZOOM*/150}; //Higher sensitivity value = slower mouse, should be <= about 500
+const int move_slow_down [4] = {/*NORMAL*/  5, /*PAN*/  5, /*ROTATE*/ 10, /*ZOOM*/ 40}; //this slows down the moves
 
 int horzPin = A0;  // Analog output of horizontal joystick pin
 int vertPin = A1;  // Analog output of vertical joystick pin
@@ -31,25 +33,24 @@ int b1Pin   =  7;  // button 1
 int b2Pin   =  8;  // button 2
 int b3Pin   =  9;  // button 3
 
-spacemouse_mode_t default_mode = NORMAL; //selects mouse mode: normal, zoom or rotate by default
-spacemouse_mode_t mode = NORMAL;
-int moved = 0;                  //checks if the joystick has moved
-int pressed = 0;                //checks if the joystick has been pressed
+spacemouse_mode_t default_mode = NORMAL; // selects mouse default mode: normal, pan, zoom or rotate
+spacemouse_mode_t mode         = NORMAL;
+int moved = 0;                  // checks if the joystick has moved
+int pressed = 0;                // checks if the joystick has been pressed
 
-int vertZero, horzZero;         //Stores the initial value of each axis, usually around 512
-int vertValue, horzValue;       //Stores current analog output of each axis
+int vertZero, horzZero;         // Stores the initial value of each axis, usually around 512
+int vertValue, horzValue;       // Stores current analog output of each axis
 
-int mouseClickFlag = 0;         //another click check
-int b1flag = 0;                 //click check
-int b2flag = 0;                 //click check
-int b3flag = 0;                 //click check
-int used = 0;                   //check if const are shown
-int lastused = 0;               //check if const are shown
+int mouseClickFlag = 0;         // another click check
+int b1flag = 0;                 // click check
+int b2flag = 0;                 // click check
+int b3flag = 0;                 // click check
+int b2toggle = 0;               // b2 toggle count
 
-unsigned long releasetime = 0;  //used to register the releasing time (to select home or zoom)
-unsigned long presstime = 0;    //as above
-int registertime = 0;           //used to selective enter the release mode
-unsigned long diff = 0;         //difference between time checks to read quick or long press
+unsigned long releasetime = 0;  // used to register the releasing time (to select home or zoom)
+unsigned long presstime = 0;    // as above
+int registertime = 0;           // used to selective enter the release mode
+unsigned long diff = 0;         // difference between time checks to read short or long press
 
 
 void setup()
@@ -69,8 +70,8 @@ void setup()
   vertZero = analogRead(vertPin);  // get the initial values
   horzZero = analogRead(horzPin);  // Joystick should be in neutral position when reading these
 
-  Mouse.begin();                   //Init mouse emulation
-  Keyboard.begin();                //Init keyboard emulation
+  Mouse.begin();                   // Init mouse emulation
+  Keyboard.begin();                // Init keyboard emulation
 #ifdef DEBUG
   Serial.begin(9600);
 #endif
@@ -94,35 +95,29 @@ void loop()
 
 //BUTTON 1
 
-  if ((digitalRead(b1Pin) == 0) && (!b1flag))  // if button 1 is pressed
+  if ((digitalRead(b1Pin) == 0) && (!b1flag)) // if button 1 is pressed
   {
     b1flag = 1;
-    used = 1;
-
-    if (used != lastused) {
-      default_mode = ROTATE;
-    } else {
-      default_mode = NORMAL;
-      used = 0;
-    }
-    mode = default_mode;
-
+    Mouse.press(MOUSE_LEFT);
   }
-  else if (digitalRead(b1Pin) && (b1flag))
+  else if (digitalRead(b1Pin) && (b1flag))    // if button 1 is released
   {
     b1flag = 0;
+    Mouse.release(MOUSE_LEFT);
   }
-  lastused = used;
 
 
 //BUTTON 2
 
-  if ((digitalRead(b2Pin) == 0) && (!b2flag))  // if button 2 is pressed
+  if ((digitalRead(b2Pin) == 0) && (!b2flag)) // if button 2 is pressed
   {
     b2flag = 1;
-    Keyboard.write(KEY_HOME); // fit zoom
+    b2toggle++;
+    b2toggle%=3;
+    default_mode = (spacemouse_mode_t)b2toggle;
+    mode = default_mode;
   }
-  else if (digitalRead(b1Pin) && (b2flag))
+  else if (digitalRead(b2Pin) && (b2flag))    // if button 2 is released
   {
     b2flag = 0;
   }
@@ -130,12 +125,12 @@ void loop()
 
 //BUTTON 3
 
-  if ((digitalRead(b3Pin) == 0) && (!b3flag))  // if button 3 is pressed
+  if ((digitalRead(b3Pin) == 0) && (!b3flag)) // if button 3 is pressed
   {
     b3flag = 1;
-    Keyboard.write(KEY_F7); //slice graphics (INVENTOR ONLY)
+    Keyboard.write(KEY_ESC); // sends ESC key
   }
-  else if (digitalRead(b3Pin) && (b3flag))
+  else if (digitalRead(b3Pin) && (b3flag))    // if button 3 is released
   {
     b3flag = 0;
   }
@@ -159,11 +154,11 @@ void loop()
     pressed = 0;
     mode = default_mode;
 
-    diff = (releasetime - presstime);  //check if the press was quick or long
+    diff = (releasetime - presstime);  // check if the press was short or long
 
-    if(diff < LONG_PRESS_INTERVAL)     //if quick press: call home view
+    if(diff < LONG_PRESS_INTERVAL)     // if short press
     {
-      Keyboard.write(KEY_F6); // home view
+      Keyboard.write(KEY_F6); // Fusion 360: zoom to fit
     }
   }
 #ifdef DEBUG
@@ -174,56 +169,11 @@ void loop()
   Serial.println(" ");
 #endif
 
-  switch(mode) {   //rotate or zoom
-
-    case ROTATE:
-
-      invertMouse = -(INVERT_MOUSE); //restore mouse orientation
-
-      if ( (vertValue <= MOVE_THRESHOLD)&&(vertValue >= (-MOVE_THRESHOLD)) )
-        vertValue = 0;
-      if ( (horzValue <= MOVE_THRESHOLD)&&(horzValue >= (-MOVE_THRESHOLD)) )
-        horzValue = 0;
-      if ( (horzValue != 0) || (vertValue != 0)) {
-        Keyboard.press(KEY_F4); //rotate view (inventor mode)
-        Mouse.press(MOUSE_LEFT);
-        Mouse.move((invertMouse * (horzValue / sensitivity[NORMAL])), (invertMouse * (vertValue / sensitivity[NORMAL])), 0); // move mouse with f4 key pressed to rotate view
-        moved = 1;
-      }
-      else {  // joystick is in neutral position: release all and stroke ESC to help go back to design view
-        if (moved == 1) {
-          Keyboard.releaseAll();
-          Mouse.release(MOUSE_LEFT);
-          Keyboard.write(KEY_ESC);
-          moved = 0;
-        }
-      }
-      break;
-
-    case ZOOM:
-
-      invertMouse = (INVERT_MOUSE);  //mouse inverted to push for zoom in and pull for zoom out
-
-      if ( (vertValue > MOVE_THRESHOLD)||(vertValue < (-MOVE_THRESHOLD)) ){
-        Keyboard.press(KEY_F3); // zoom view
-        Mouse.press(MOUSE_LEFT);
-        Mouse.move(0, (invertMouse * (vertValue / sensitivity[ZOOM])), 0); // move mouse on y axis with f3 key pressed to zoom view
-        moved=1;
-      }
-
-      if ( (vertValue <= MOVE_THRESHOLD)&&(vertValue >= (-MOVE_THRESHOLD))  &&  (horzValue <= MOVE_THRESHOLD)&&(horzValue >= (-MOVE_THRESHOLD))  ){
-          if (moved == 1) {
-            Keyboard.releaseAll();
-            Mouse.release(MOUSE_LEFT);
-            Keyboard.write(KEY_ESC);
-            moved = 0;
-          }
-      }
-      break;
+  switch(mode) {   // normal, pan, rotate or zoom
 
     case NORMAL:
 
-      invertMouse = -(INVERT_MOUSE); //restore mouse orientation
+      invertMouse = -(1 - 2 * INVERT_MOUSE); // restore mouse orientation
 
       if ( (vertValue <= MOVE_THRESHOLD)&&(vertValue >= (-MOVE_THRESHOLD)) )
         vertValue = 0;
@@ -233,8 +183,72 @@ void loop()
         Mouse.move((invertMouse * (horzValue / sensitivity[NORMAL])), (invertMouse * (vertValue / sensitivity[NORMAL])), 0); // move mouse
 
       break;
+
+    case PAN:
+
+      invertMouse = -(1 - 2 * INVERT_MOUSE); // restore mouse orientation
+
+      if ( (vertValue <= MOVE_THRESHOLD)&&(vertValue >= (-MOVE_THRESHOLD)) )
+        vertValue = 0;
+      if ( (horzValue <= MOVE_THRESHOLD)&&(horzValue >= (-MOVE_THRESHOLD)) )
+        horzValue = 0;
+      if ( (horzValue != 0) || (vertValue != 0)) {
+        Mouse.press(MOUSE_MIDDLE);
+        Mouse.move((invertMouse * (horzValue / sensitivity[PAN])), (invertMouse * (vertValue / sensitivity[PAN])), 0); // move mouse with middle button pressed to pan view
+        moved = 1;
+      }
+      else {  // joystick is in neutral position: release all and stroke ESC to help go back to design view
+        if (moved == 1) {
+          Mouse.release(MOUSE_MIDDLE);
+          //Keyboard.write(KEY_ESC);
+          moved = 0;
+        }
+      }
+      break;
+
+    case ROTATE:
+
+      invertMouse = -(1 - 2 * INVERT_MOUSE); //restore mouse orientation
+
+      if ( (vertValue <= MOVE_THRESHOLD)&&(vertValue >= (-MOVE_THRESHOLD)) )
+        vertValue = 0;
+      if ( (horzValue <= MOVE_THRESHOLD)&&(horzValue >= (-MOVE_THRESHOLD)) )
+        horzValue = 0;
+      if ( (horzValue != 0) || (vertValue != 0)) {
+        Keyboard.press(KEY_LEFT_SHIFT); //rotate view (fusion 360 mode)
+        Mouse.press(MOUSE_MIDDLE);
+        Mouse.move((invertMouse * (horzValue / sensitivity[NORMAL])), (invertMouse * (vertValue / sensitivity[NORMAL])), 0); // move mouse with f4 key pressed to rotate view
+        moved = 1;
+      }
+      else {  // joystick is in neutral position: release all and stroke ESC to help go back to design view
+        if (moved == 1) {
+          Keyboard.releaseAll();
+          Mouse.release(MOUSE_MIDDLE);
+          //Keyboard.write(KEY_ESC);
+          moved = 0;
+        }
+      }
+      break;
+
+    case ZOOM:
+
+      invertMouse = (1 - 2 * INVERT_MOUSE);  //mouse inverted to push for zoom in and pull for zoom out
+
+      if ( (vertValue > MOVE_THRESHOLD)||(vertValue < (-MOVE_THRESHOLD)) ){
+        Mouse.move(0, 0, (invertMouse * (vertValue / sensitivity[ZOOM]))); // move mouse on y axis with scroll wheel to zoom view
+        moved=1;
+      }
+
+      if ( (vertValue <= MOVE_THRESHOLD)&&(vertValue >= (-MOVE_THRESHOLD))  &&  (horzValue <= MOVE_THRESHOLD)&&(horzValue >= (-MOVE_THRESHOLD))  ){
+          if (moved == 1) {
+            //Nothing to do
+            moved = 0;
+          }
+      }
+      break;
+
     }
 
-  delay(MOVE_SLOW_DOWN); //this slows down the moves
+  delay(move_slow_down[mode]); //this slows down the moves
 
 }
